6d7591b65d96433add09356d1b86048dfce01d0b
Shivam Gupta <shivamgupta02005@gmail.com>
2023-08-16 22:49:27 +0530

    Add play function

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
 function getComputerChoice(){
     choices = ['Rock','Paper','Scissor']
     choice = Math.floor(Math.random()*3)
-    console.log(choice)
     return choices[choice]
 }
 
-console.log(getComputerChoice())
+function play(){
+    playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+    computerSelection = getComputerChoice().toLowerCase();
+    console.log(computerSelection);
+    console.log(playerSelection);
+    switch (computerSelection){
+        case playerSelection:
+            console.log("It's a tie!");
+            break;
+        case 'rock':
+            if (playerSelection=='paper'){
+                console.log('You won!');
+            } else{
+                console.log('You Lost!');
+            } break;
+        case 'paper':
+            if (playerSelection=='scissor'){
+                console.log('You won!');
+            } else{
+                console.log('You Lost!');
+            } break;
+        case 'scissor':
+            if (playerSelection=='rock'){
+                console.log('You won!');
+            } else{
+                console.log('You Lost!');
+            } break;
+    }
+}
+console.log("hello");
+play();
