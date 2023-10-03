fa5a3704690c3a27344a4979a2de5e5eb557b160
Shivam Gupta <shivamgupta02005@gmail.com>
2023-10-04 02:57:13 +0530

    Moved the game from console to dom

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
+const rock_btn =document.querySelector('button.rock');
+const paper_btn =document.querySelector('button.paper');
+const scissor_btn =document.querySelector('button.scissors');
+const result_div = document.querySelector('.result');
+const result_heading = document.createElement('h2');
+result_div.appendChild(result_heading);
+
+rock_btn.addEventListener('click', playerChoice);
+paper_btn.addEventListener('click', playerChoice);
+scissor_btn.addEventListener('click', playerChoice);
+function playerChoice(event){
+    if (event.target.classList.contains('rock')){
+        playRound('rock');
+    }else if (event.target.classList.contains('paper')){
+        playRound('paper');
+    }else if (event.target.classList.contains('scissors')){
+        playRound('scissors');
+    }
+}
+
+
 function getComputerChoice(){
-    choices = ['Rock','Paper','Scissor']
+    choices = ['rock','paper','scissors']
     choice = Math.floor(Math.random()*3)
     return choices[choice]
 }
@@ -11,21 +32,21 @@ function play(playerSelection, computerSelection){
             break;
         case 'rock':
             if (playerSelection=='paper'){
-                return(`You won${playerSelection} beats ${computerSelection}`);
+                return(`You won ${playerSelection} beats ${computerSelection}`);
             } else{
                 return(`You lose ${computerSelection} beats ${playerSelection}`);
             }
             break;
         case 'paper':
-            if (playerSelection=='scissor'){
-                return(`You won${playerSelection} beats ${computerSelection}`);
+            if (playerSelection=='scissors'){
+                return(`You won ${playerSelection} beats ${computerSelection}`);
             } else{
                 return(`You lose ${computerSelection} beats ${playerSelection}`);
             }
             break;
-        case 'scissor':
+        case 'scissors':
             if (playerSelection=='rock'){
-                return(`You won${playerSelection} beats ${computerSelection}`);
+                return(`You won ${playerSelection} beats ${computerSelection}`);
             } else{
                 return(`You lose ${computerSelection} beats ${playerSelection}`);
             }
@@ -33,19 +54,36 @@ function play(playerSelection, computerSelection){
     }
 }
 
-function game(){
-    let score = 0;
-    for (let i=0; i<5; i++){
-        const playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
-        const computerSelection = getComputerChoice().toLowerCase();
-        let result = play(playerSelection, computerSelection);
-        console.log(result);
-        if (result.includes('won')){
-            score+=1;
-        }
-        console.log(score);
+
+let computer_score = 0;
+let player_score = 0;
+function playRound(player_choice){
+    const playerSelection = player_choice;
+    const computerSelection = getComputerChoice().toLowerCase();
+    let result = play(playerSelection, computerSelection);
+        
+    if (result.includes('won')){
+        player_score+=1;
+    }else if (result.includes('lose')){
+        computer_score+=1;
     }
-    
+    result_heading.textContent = `${result}!!!
+    Player Score: ${player_score}
+    Computer Score: ${computer_score}`;
+
+    if (player_score == 5){
+        result_heading.textContent = `YOU WIN COMPUTER LOSES! 
+        Final score: 
+        Computer: ${computer_score} Player: ${player_score}`;
+        computer_score = 0;
+        player_score = 0;
+    }else if(computer_score == 5){
+        result_heading.textContent = `YOU LOSE COMPUTER WINS! 
+        Final score: 
+        Computer: ${computer_score} Player: ${player_score}`;
+        computer_score = 0;
+        player_score = 0;
+    }
 }
 
-// game();
+
